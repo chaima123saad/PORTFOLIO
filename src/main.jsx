4c723f1be87fbac2f385488b9c453e8aa46ddff9
Chaima Saad <chaima.saad.2202@gmail.com>
2024-09-11 12:13:14 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {BrowserRouter, Route,Routes} from "react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/PORTFOLIO/">
+    <Routes>
+      <Route path="*" element={<App />}/>
+    </Routes>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
